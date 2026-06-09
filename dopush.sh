84909640141954bb936271f814fb78d99216dbe4
Mkt Web 360 <mktweb360@gmail.com>
2026-06-09 13:44:19 +0200

    chore: final cleanup

--- dopush.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH="/usr/bin:/home/mktwe/.local/share/fnm/node-versions/v22.22.3/installation/bin:$PATH"
-cd /home/mktwe/guiadelpiscina
-git push -u origin main 2>&1
-echo "Exit code: $?"
-echo "COMMIT: $(git rev-parse HEAD)"
